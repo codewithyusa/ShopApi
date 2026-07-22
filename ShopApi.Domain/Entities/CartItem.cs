b41ad2e80b985dc0b25ed3169564f8a7fdee67b5
Yusuf Umer <yusufumer26@gmail.com>
2026-07-22 14:36:23 +0300

    feat(domain): add CartItem entity

---
+++ ShopApi.Domain/Entities/CartItem.cs
@@ -0,0 +1,13 @@
+namespace ShopApi.Domain.Entities;
+
+public class CartItem
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; } = 1;
+
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+}
