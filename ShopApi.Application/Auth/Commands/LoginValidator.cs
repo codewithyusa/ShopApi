6b2e058a8118b80a7e6e46e5bd7fd3ec59f7b3f0
Yusuf Umer <yusufumer26@gmail.com>
2026-07-23 22:54:10 +0300

    feat(application): add login validation rules

---
+++ ShopApi.Application/Auth/Commands/LoginValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace ShopApi.Application.Auth.Commands;
+
+public class LoginValidator : AbstractValidator<LoginCommand>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
