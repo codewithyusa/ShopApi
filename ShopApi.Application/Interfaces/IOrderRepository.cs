3fdaacdd46a7d958fa14b10ea6a8781f69dc2ba0
Yusuf Umer <yusufumer26@gmail.com>
2026-07-25 17:49:15 +0300

    feat(application): add order lookup by payment reference

--- ShopApi.Application/Interfaces/IOrderRepository.cs
+++ ShopApi.Application/Interfaces/IOrderRepository.cs
@@ -7,6 +7,7 @@ public interface IOrderRepository
     Task<Order?> GetByIdAsync(int id, CancellationToken ct);
     Task<List<Order>> GetByUserIdAsync(int userId, CancellationToken ct);
     Task<List<Order>> GetAllAsync(CancellationToken ct);
+    Task<Order?> GetByPaymentRefAsync(string paymentRef, CancellationToken ct);
     Task AddAsync(Order order, CancellationToken ct);
     Task SaveChangesAsync(CancellationToken ct);
 }
