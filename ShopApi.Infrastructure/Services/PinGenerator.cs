62c2292ec3cd966c0994f654114a93d38d4f599c
Yusuf Umer <yusufumer26@gmail.com>
2026-07-26 21:37:37 +0300

    feat: add PIN generator service

---
+++ ShopApi.Infrastructure/Services/PinGenerator.cs
@@ -0,0 +1,7 @@
+namespace ShopApi.Infrastructure.Services;
+
+public static class PinGenerator
+{
+    public static string Generate(int length = 6) =>
+        Random.Shared.Next(0, (int)Math.Pow(10, length)).ToString(new string('0', length));
+}
