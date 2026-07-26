fc34b7a364d79461e6942ad56fa0e42ec92fde94
Yusuf Umer <yusufumer26@gmail.com>
2026-07-26 22:13:31 +0300

    feat: add email verification database migration

---
+++ ShopApi.Infrastructure/Migrations/20260726191258_AddEmailVerification.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationCode",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailVerificationExpires",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailVerified",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationExpires",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailVerified",
+                table: "Users");
+        }
+    }
+}
