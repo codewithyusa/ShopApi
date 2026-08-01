9f90cfc4fdfd3814740221464c42b021cb659b9b
Yusuf Umer <yusufumer26@gmail.com>
2026-08-01 22:44:48 +0300

    feat(analytics): add top selling products repository method

--- ShopApi.Application/Interfaces/IAnalyticsRepository.cs
+++ ShopApi.Application/Interfaces/IAnalyticsRepository.cs
@@ -6,4 +6,6 @@ public interface IAnalyticsRepository
 {
     Task<AnalyticsSummaryDto> GetSummaryAsync(CancellationToken ct);
     Task<List<DailySalesDto>> GetDailySalesAsync(int days, CancellationToken ct);
+    Task<List<TopSellingProductDto>> GetTopSellingProductsAsync(int topN, CancellationToken ct);
+    Task<List<CouponUsageDto>> GetCouponUsageAsync(CancellationToken ct);
 }
