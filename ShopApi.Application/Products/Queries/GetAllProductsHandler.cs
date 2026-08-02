49149e7312369325d77cdcdbb8ed89ab0f0c5b2b
Yusuf Umer <yusufumer26@gmail.com>
2026-08-02 12:02:49 +0300

    feat(products): implement paged product query handler

--- ShopApi.Application/Products/Queries/GetAllProductsHandler.cs
+++ ShopApi.Application/Products/Queries/GetAllProductsHandler.cs
@@ -1,16 +1,24 @@
 using MediatR;
+using ShopApi.Application.Common;
 using ShopApi.Application.Interfaces;
 using ShopApi.Application.Products.Dtos;
 
 namespace ShopApi.Application.Products.Queries;
 
 public class GetAllProductsHandler(IProductRepository products)
-    : IRequestHandler<GetAllProductsQuery, List<ProductResponseDto>>
+    : IRequestHandler<GetAllProductsQuery, PagedResponse<ProductResponseDto>>
 {
-    public async Task<List<ProductResponseDto>> Handle(GetAllProductsQuery query, CancellationToken ct)
+    public async Task<PagedResponse<ProductResponseDto>> Handle(GetAllProductsQuery query, CancellationToken ct)
     {
-        var list = await products.GetAllAsync(ct);
-        return list.Select(ToDto).ToList();
+        var (items, totalCount) = await products.GetPagedAsync(query.Paging, ct);
+
+        return new PagedResponse<ProductResponseDto>
+        {
+            Items = items.Select(ToDto).ToList(),
+            TotalCount = totalCount,
+            Page = query.Paging.Page,
+            PageSize = query.Paging.PageSize
+        };
     }
 
     public static ProductResponseDto ToDto(Domain.Entities.Product p) =>
