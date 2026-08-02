19bd794440449f160c5756ade4ee25892901cc5b
Yusuf Umer <yusufumer26@gmail.com>
2026-08-02 12:17:35 +0300

    feat(orders): add paged orders query

--- ShopApi.Application/Orders/Queries/GetAllOrdersQuery.cs
+++ ShopApi.Application/Orders/Queries/GetAllOrdersQuery.cs
@@ -1,6 +1,7 @@
 using MediatR;
+using ShopApi.Application.Common;
 using ShopApi.Application.Orders.Dtos;
 
 namespace ShopApi.Application.Orders.Queries;
 
-public record GetAllOrdersQuery : IRequest<List<OrderResponseDto>>;
+public record GetAllOrdersQuery(PagedRequest Paging) : IRequest<PagedResponse<OrderResponseDto>>;
