49660a0c36476189fe927638544317e2b59a7919
Yusuf Umer <yusufumer26@gmail.com>
2026-08-02 12:02:15 +0300

    feat(products): update product query for pagination

--- ShopApi.Application/Products/Queries/GetAllProductsQuery.cs
+++ ShopApi.Application/Products/Queries/GetAllProductsQuery.cs
@@ -1,6 +1,7 @@
 using MediatR;
+using ShopApi.Application.Common;
 using ShopApi.Application.Products.Dtos;
 
 namespace ShopApi.Application.Products.Queries;
 
-public record GetAllProductsQuery : IRequest<List<ProductResponseDto>>;
+public record GetAllProductsQuery(PagedRequest Paging) : IRequest<PagedResponse<ProductResponseDto>>;
