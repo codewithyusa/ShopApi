59ff3fb9e74b4b2591be6607fc9e6047bac66eb1
Yusuf Umer <yusufumer26@gmail.com>
2026-08-02 12:03:54 +0300

    feat(products): implement paged search handler

--- ShopApi.Application/Products/Queries/SearchProductsHandler.cs
+++ ShopApi.Application/Products/Queries/SearchProductsHandler.cs
@@ -1,17 +1,24 @@
 using MediatR;
+using ShopApi.Application.Common;
 using ShopApi.Application.Interfaces;
 using ShopApi.Application.Products.Dtos;
 
 namespace ShopApi.Application.Products.Queries;
 
 public class SearchProductsHandler(IProductRepository products)
-    : IRequestHandler<SearchProductsQuery, List<ProductResponseDto>>
+    : IRequestHandler<SearchProductsQuery, PagedResponse<ProductResponseDto>>
 {
-    public async Task<List<ProductResponseDto>> Handle(SearchProductsQuery query, CancellationToken ct)
+    public async Task<PagedResponse<ProductResponseDto>> Handle(SearchProductsQuery query, CancellationToken ct)
     {
-        var results = await products.SearchAsync(
-            query.Name, query.Category, query.MinPrice, query.MaxPrice, query.InStockOnly, ct);
+        var (items, totalCount) = await products.SearchPagedAsync(
+            query.Name, query.Category, query.MinPrice, query.MaxPrice, query.InStockOnly, query.Paging, ct);
 
-        return results.Select(GetAllProductsHandler.ToDto).ToList();
+        return new PagedResponse<ProductResponseDto>
+        {
+            Items = items.Select(GetAllProductsHandler.ToDto).ToList(),
+            TotalCount = totalCount,
+            Page = query.Paging.Page,
+            PageSize = query.Paging.PageSize
+        };
     }
 }
