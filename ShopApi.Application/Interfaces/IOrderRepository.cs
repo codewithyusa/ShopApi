1ef4623f98cace8c478662ce6e62cff08efab289
Yusuf Umer <yusufumer26@gmail.com>
2026-08-02 12:16:54 +0300

    feat(orders): add paged order repository contract

--- ShopApi.Application/Interfaces/IOrderRepository.cs
+++ ShopApi.Application/Interfaces/IOrderRepository.cs
@@ -1,3 +1,4 @@
+using ShopApi.Application.Common;
 using ShopApi.Domain.Entities;
 
 namespace ShopApi.Application.Interfaces;
@@ -7,6 +8,7 @@ public interface IOrderRepository
     Task<Order?> GetByIdAsync(int id, CancellationToken ct);
     Task<List<Order>> GetByUserIdAsync(int userId, CancellationToken ct);
     Task<List<Order>> GetAllAsync(CancellationToken ct);
+    Task<(List<Order> Items, int TotalCount)> GetAllPagedAsync(PagedRequest request, CancellationToken ct);
     Task<Order?> GetByPaymentRefAsync(string paymentRef, CancellationToken ct);
     Task AddAsync(Order order, CancellationToken ct);
     Task SaveChangesAsync(CancellationToken ct);
